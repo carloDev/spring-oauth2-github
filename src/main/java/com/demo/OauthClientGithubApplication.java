65f4d039a5fe13007f6c7dcb12901f38534cb759
Costin Moraru <costin.moraru2018@gmail.com>
2020-11-04 16:02:46 +0100

    Primo commit

---
+++ src/main/java/com/demo/OauthClientGithubApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthClientGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthClientGithubApplication.class, args);
+	}
+
+}
